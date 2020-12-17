b02797a33a83bfe1bf80ac9e8ceb3a237b8ec009
Ian Hawe <ianhawe24@hotmail.co.uk>
2020-12-17 09:56:52 +0000

    removed purposely put error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,9 +4,9 @@ import { robots } from '../robots';
 
 
 const CardList = ({ robots }) => {
-  if(true){
-    throw new Error('NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!');
-  }
+  // if(true){
+  //   throw new Error('NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!');
+  // }
   return robots.map((user, i) => {
     return (
       <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
